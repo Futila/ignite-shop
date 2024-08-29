4eafdbf4a8cf2fdacb44b180434aa87209057a30
Fernando Futila <fernando.futila@gmail.com>
2024-08-28 23:54:03 -0300

    Estrutura visual - Estrutura da Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { globalStyles } from "../styles/global";
 import { Container, Header } from "../styles/pages/app";
 
 import logoImg from "../assets/logo.svg"
+import Image from "next/image";
 
 globalStyles()
 
@@ -10,12 +11,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
         <Header>
-          <img src={logoImg.src} alt="" />
+          <Image src={logoImg} alt="" />
         </Header>
-
-
-
-
         <Component {...pageProps} />
     </Container>
   )
