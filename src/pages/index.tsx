60aff0749c06bda0e7ba0e7c31ef4dc13e4c15a7
Fernando Futila <fernando.futila@gmail.com>
2024-08-28 23:20:23 -0300

    Estrutura visual - Estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,11 @@ import { styled } from "../styles";
 
 
 
-const Button = styled("button", {
-  backgroundColor:'$rocketseat', 
-  borderRadius: 4,
-  width: 400
-})
+
 
 
 export default function Home() {
   return (
-    <div>
-      <Button>Enviar</Button>
-    </div>
+    <h1>Ola mundo</h1>
   );
 }
